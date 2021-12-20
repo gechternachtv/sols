e83208b755079b9fcdfd002af0b13a8e2bb6e1e3
gechternacht <gabriel@agenciavinci.com.br>
2021-12-20 20:47:51 -0300

    pt4

--- src/Home.svelte
+++ src/Home.svelte
@@ -422,7 +422,7 @@ Sua música envolve sincretismo, miscigenação e mistura. Bases eletrônicas me
 <article class="streaming">
     {@html
     `
-    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/eWt8dToE4C4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/YGzW9VB_yK4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     `
     }
 </article>
